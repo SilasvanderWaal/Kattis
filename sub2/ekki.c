21f87897ebf7dd3faca0575688c509e60632fd45
Silas van der Waal <silasvanderwaal@gmail.com>
2023-11-15 18:39:51 +0100

    Ekki dauði opna inni problem solved

--- sub2/ekki.c
+++ sub2/ekki.c
@@ -3,18 +3,46 @@
 #define MAX 2100
 
 int main(){
-    char line1[MAX],line2[MAX], output[MAX *2];
+    char line1[MAX],line2[MAX];
     char *search = "|";
+    int counter = 0;
     
     fgets(line1, MAX,stdin);
     fgets(line2, MAX, stdin);
 
-    for (int i = 0; i < strlen(line1); i++)
+    for (int i = 0; i < strlen(line1) - 1; i++)
     {
-        while (line1[i] != '|')
+        
+        if (line1[i] == '|')
         {
-        printf("%s", &line1[i]);
+
+            for (int i = 0; i < strlen(line2); i++)
+            {
+
+                counter++;
+
+                if(line2[i] == '|')
+                {
+                    printf(" ");
+                    i = strlen(line2);
+                }
+
+                if(line2[i] != '|')
+                {
+                    printf("%c", line2[i]);
+                }
+            }
         }
+
+        if(line1[i] != '|')
+        {
+        printf("%c", line1[i]);
+        }
+    }
+
+    for (int i = counter; i < strlen(line2); i++)
+    {
+        printf("%c", line2[i]);
     }
     
     return 0;
